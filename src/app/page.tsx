0edbc2ffc33fae66b91372dbd0c0b6f663adead9
Afnan Ebrahim <afnan.ebrahim214@gmail.com>
2024-09-23 03:39:53 -0400

    Revert "login page that you guys created + firebase auth."

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,59 @@
 'use client'
 
+import { FormEvent } from "react";
 import { useRouter } from "next/navigation";
-import Link from "next/link";
-import { useAppSelector } from "@/lib/redux/hooks/redux";
-import { Button } from "@/components/ui/button";
-interface User {
-  displayName?: string,
-  uid: string,
-  email: string
-}
-export default function Login() {
-  const router = useRouter()
-  const { isAuthenticated, isLoading, user }: {
-    isAuthenticated: boolean;
-    isLoading: boolean;
-    user: User | null;
-  } = useAppSelector(state => state.auth)
-  return (
-    <main className="min-w-full flex justify-center ">
-      <div className="max-w-fit content-center">
-        {
-          user != null ? (
-            user.displayName ?
-              (
-                <h1>
-                  Welcome to unionizer {user.displayName}
-                </h1>
-              ) :
-              (<h1>
-                Welcome to unionizer {user.email}
-              </h1>))
-            :
-            <ul className="p-3 border-2 rounded-lg shadow-lg">
-              <li>
-                <Button className="min-w-56 m-2">
-                  <Link href={"/auth/login"}>Login</Link>
-                </Button>
-              </li>
-              <li>
-                <Button className="min-w-56 m-2">
-                  <Link href={"/auth/signup"}>Create an account</Link>
-                </Button>
-              </li>
 
-            </ul>
-        }
+export default function Login() { 
+  const router = useRouter()
+
+  // Boilerplate code from https://nextjs.org/docs/pages/building-your-application/authentication
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const formData = new FormData(event.currentTarget)
+    const username = formData.get('username')
+    const password = formData.get('password')
+
+    const response = await fetch('/api/auth/login', { 
+      method: 'POST',
+      headers: { 'ContentType': 'application/json'},
+      body: JSON.stringify({ username, password }),
+    })
+
+    if(response.ok) {
+      // router.push(*INSERT PAGE HERE*)
+    } else {
+      // Retry Login
+    }
+  }
+
+  return (
+    <main>
+      <div className="main">
+        <div className="top-bar-container">
+          <img src="" alt="Logo" id="logo" />
+          <div className="about-us">
+            <a href="">About us</a>
+          </div>
+        </div>
+        <div className = "login-center-container">
+          <div className="login-form-container">
+            <h1>Log In</h1>
+            <form className="login-form" onSubmit={handleSubmit}>
+              <div className="username-form">
+                <input id="login-username" name="username" type="username" placeholder="Username" required />
+              </div>
+              <div className="password-form">
+                <input id="login-password" name="password" type="password" placeholder="Password" required />
+              </div>
+            </form>
+            <h3><a href="" id="forgot-password">Forgot Password?</a></h3>
+            <button type="submit" id="login-button">Log in</button>
+          </div>
+          <div className="create-account-container">
+            <h3 id="new-to-unionizer">New to Unionizer? <a href="" id="join-now">Join now</a></h3>
+          </div>
+        </div>
       </div>
     </main>
   );
